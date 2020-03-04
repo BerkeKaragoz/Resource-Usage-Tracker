9f9bd38d9ffb07fd54f41940eefc9f51f7acbb26
BerkeKaragoz <e.berkekaragoz@gmail.com>
2020-03-04 08:16:49 +0300

    CPU Usage

--- resource_usage_tracker.c
+++ resource_usage_tracker.c
@@ -3,9 +3,17 @@
 #include <string.h>
 #include <time.h>
 
-#define STRLEN(X) sizeof(X)-1
+#define KILOBYTE 1024
+#define MEGABYTE KILOBYTE * KILOBYTE
+#define GIGABYTE KILOBYTE * MEGABYTE
 
-#define FILENAME "/proc/stat"
+#define PATH_CPU_STATS "/proc/stat"
+#define PATH_DISK_STATS "/proc/diskstats"
+#define PATH_MEM_STATS "/proc/meminfo"
+
+#define PASS_ARR(X) X, sizeof(X)
+#define REQUEST_ARR(X) X[], const unsigned int key_size
+
 
 void sleep_ms(int milliseconds) 
 {
@@ -39,13 +47,13 @@ char* run_command(char* command){
 	return output;
 }
 
-char* readCpuStat(const char cpu_d[]){
-	const char cpu_id[5] = "cpu ";
-	const unsigned int buffer_size = 1024;
+// Read the first subsequent lines at PATH starting with SEARCH_KEY
+char* readSearchFirstOccrLines(const char* path, REQUEST_ARR(const char search_key)){
+	const unsigned int buffer_size = KILOBYTE;
 
-	FILE *fp = fopen(FILENAME, "r");
+	FILE *fp = fopen(path, "r");
 	if (fp == NULL){
-		return strcat("Could not read the file: ", FILENAME);
+		return strcat("Could not read the file: ", path);
 	}
 	char *output = NULL, *line = NULL;
 	size_t output_size = buffer_size;
@@ -53,7 +61,7 @@ char* readCpuStat(const char cpu_d[]){
 	line = (char *)malloc(buffer_size);
 	*output = '\0';
 
-	while( fgets(line, buffer_size, fp) != NULL && !strncmp(line, cpu_id, STRLEN(cpu_id)) ) {
+	while( fgets(line, buffer_size, fp) != NULL && !strncmp(line, search_key, key_size-1) ) {
 		output_size *= 2;
 		output = realloc(output, output_size);
 		strcat(output, line);
@@ -70,7 +78,7 @@ void getCpuTimings(unsigned long *cpu_total, unsigned long *cpu_idle){
 	unsigned char i = 0;
 	char* token;
 
-	token = strtok(readCpuStat("cpu "), delim);
+	token = strtok(readSearchFirstOccrLines(PATH_CPU_STATS, PASS_ARR("cpu ")), delim);
 	token = strtok(NULL, delim); // skip cpu_id
 
 	while ( token != NULL){
@@ -80,15 +88,15 @@ void getCpuTimings(unsigned long *cpu_total, unsigned long *cpu_idle){
 			*cpu_idle = atol(token);
 		}
 	}
-
 }
 
 // 1000 ms is stable
+// cat <(grep cpu /proc/stat) <(sleep 0.1 && grep cpu /proc/stat) | awk -v RS="" '{printf "%.1f", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}
 long double getCpuUsage(const unsigned int ms_interval){
 
 	unsigned long 	total = 0, idle = 0,
 					prev_total = 0, prev_idle = 0;
-	long double 	cpu_idle_fraction = 0, usage = 0;
+	long double 	usage = 0;
 
 	getCpuTimings(&prev_total, &prev_idle);
 	sleep_ms(ms_interval);
@@ -102,12 +110,9 @@ long double getCpuUsage(const unsigned int ms_interval){
 }
 
 int main (){
-	const int interval = 100;
+	const int interval = 1000; // CHANGE IT TO 1000
 
 	getCpuUsage(interval);
 
 	return 0;
 }
-
-// cat <(grep cpu /proc/stat) <(sleep 0.1 && grep cpu /proc/stat) | awk -v RS="" '{printf "%.1f", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}
-// cat <(grep cpu /proc/stat) <(sleep 0.1 && grep cpu /proc/stat) | awk -v RS="" '{printf "%.1f\t%.1f\n", $13, $2}'
